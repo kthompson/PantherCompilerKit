8020e8a78ef0d49bbf1d76e2234839ca202cbd12
Kevin Thompson <kevin@theautomaters.com>
2025-01-14 22:56:04 -0800

    refactor: break up classes into separate files

---
+++ pnc/src/SymbolKind.pn
@@ -0,0 +1,13 @@
+enum SymbolKind {
+  case Namespace
+  case Object
+  case Class
+  case Enum
+
+  case TypeParameter(variance: Variance)
+
+  // Typed symbols
+  case Field
+  case Method
+  case Parameter
+}
---
+++ pncs/src/SymbolTreePrinter.scala
@@ -0,0 +1,68 @@
+import AstPrinter._
+import panther._
+
+case class SymbolTreePrinter(binder: Binder) {
+  def printSymbols(symbols: List[Symbol], indent: string): unit = {
+    symbols match {
+      case List.Nil => ()
+      case List.Cons(head, tail) =>
+        _printSymbol(head, indent, tail.isEmpty())
+        if (!tail.isEmpty()) {
+          printSymbols(tail, indent)
+        }
+    }
+  }
+
+  def printSymbol(symbol: Symbol): unit = _printSymbol(symbol, "", true)
+
+  def _printSymbol(symbol: Symbol, indent: string, last: bool): unit = {
+    val marker = if (last) "└──" else "├──"
+
+    printColor(ColorPalette.Comment)
+    print(indent)
+    print(marker)
+    print(ANSI.Clear)
+
+    val kind = symbol.kind
+    AstPrinter.printSymbolKind(kind)
+    print(" ")
+
+    if (symbol.kind == SymbolKind.Class || symbol.kind == SymbolKind.Object) {
+      print(symbol.qualifiedName())
+    } else {
+      print(symbol.name)
+    }
+    printColor(ColorPalette.Punctuation)
+
+    if (
+      symbol.kind != SymbolKind.Namespace && symbol.kind != SymbolKind.Object
+    ) {
+      val typ = binder.getSymbolType(symbol)
+      typ match {
+        case Option.Some(value) =>
+          symbol.kind match {
+            case SymbolKind.Class            =>
+            case SymbolKind.TypeParameter(_) =>
+            case _ =>
+              print(": ")
+              printType(value)
+          }
+        case Option.None =>
+          print(": ")
+          printColor(ColorPalette.Error)
+          print("[missing type]")
+          print(ANSI.Clear)
+      }
+    }
+
+    println()
+
+    val members = symbol.members()
+    if (!members.isEmpty()) {
+      val end = if (last) "    " else "│   "
+      val childIndent = indent + end
+      printSymbols(members, childIndent)
+    }
+  }
+
+}
